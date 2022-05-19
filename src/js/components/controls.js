42427dca031eaa620c2cc4ffd771fcd95407349c
DanielSobrino <dsobrino343@gmail.com>
2022-05-19 19:44:23 +0200

    Neuronal network created

--- src/js/components/controls.js
+++ src/js/components/controls.js
@@ -1,11 +1,18 @@
 class Controls {
-    constructor() {
+    constructor(type) {
         this.forward = false;
         this.left = false;
         this.right = false;
         this.reverse = false;
 
-        this.#addKeyboardListenners();
+        switch (type) {
+            case 'KEYS':
+                this.#addKeyboardListenners();
+                break;
+            case 'DUMMY':
+                this.forward = true;
+                break;
+        }
     }
 
     #addKeyboardListenners() {
