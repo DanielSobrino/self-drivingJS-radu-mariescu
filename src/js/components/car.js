7df80d6848a912a595c61af3e019ef20450ef661
DanielSobrino <dsobrino343@gmail.com>
2022-05-21 13:48:02 +0200

    It now learns!

--- src/js/components/car.js
+++ src/js/components/car.js
@@ -120,7 +120,7 @@ class Car {
         this.y -= Math.cos(this.angle) * this.speed;
     }
 
-    draw(ctx, color) {
+    draw(ctx, color, drawSensor = false) {
         if (this.damaged) {
             ctx.fillStyle = 'gray';
         } else {
@@ -132,7 +132,7 @@ class Car {
             ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
         }
         ctx.fill();
-        if (this.sensor) {
+        if (this.sensor && drawSensor) {
             this.sensor.draw(ctx);
         }
     }
